bd73154a83b4d000ab4afd712fb1cc1c32da1c30
lucas1487 <lucas1487@gmail.com>
2022-10-18 13:45:35 -0300

    desktop

--- main.js
+++ main.js
@@ -1,19 +1,9 @@
 
 const btnOtra=document.getElementById('menu');
 btnOtra.addEventListener('click', ()=>{
-    let ocultarContenido=document.getElementById('main');
+    let ocultarContenido=document.getElementById('contenidoGral');
         ocultarContenido.style.display='none';
-        let ocultarContenido2=document.getElementById('primeraSeccion');
-        ocultarContenido2.style.display='none';
-        let ocultarContenido3=document.getElementById('segundaSeccion');
-        ocultarContenido3.style.display='none';
-        let ocultarContenido4=document.getElementById('terceraSeccion');
-        ocultarContenido4.style.display='none';
-        let ocultarContenido5=document.getElementById('footer');
-        ocultarContenido5.style.display='none';
-        
-        let ocultarContenido6=document.getElementById('cuartaSeccion');
-        ocultarContenido6.style.display='none';
+       
         
         let mostrarContenido=document.getElementById('pagina');
         mostrarContenido.style.display='flex';
@@ -25,10 +15,9 @@ const btnClose=document.getElementById('closeMenu');
 btnClose.addEventListener('click', ()=>{
     let ocultarContenido=document.getElementById('pagina');
         ocultarContenido.style.display='none';
-        let mostrarContenido=document.getElementById('main');
+        let mostrarContenido=document.getElementById('contenidoGral');
         mostrarContenido.style.display='flex';
-        let mostrarContenido2=document.getElementById('primeraSeccion');
-        mostrarContenido2.style.display='block';
+        
 
 
 })
@@ -38,10 +27,9 @@ const btnProd=document.getElementById('productos');
 btnProd.addEventListener('click', ()=>{
     let ocultarContenido=document.getElementById('pagina');
         ocultarContenido.style.display='none';
-        let mostrarContenido=document.getElementById('primeraSeccion');
+        let mostrarContenido=document.getElementById('contenidoGral');
         mostrarContenido.style.display='block';
-        let mostrarContenido2=document.getElementById('main');
-        mostrarContenido2.style.display='flex';
+    
         
 
 
